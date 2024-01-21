33b5eda532d220702d11bf0ad73d606807399cf5
Max Landon <lavamarmax@gmail.com>
2024-01-20 21:22:38 -0800

    Implement speech to text

--- textToSpeechService/server.py
+++ textToSpeechService/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, send_file, request
-from textToSpeech import generateSpeech
+from flask import Flask, send_file, request, jsonify
+from textToSpeech import generateSpeech, generateText
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +14,18 @@ def tts():
     speech = generateSpeech("output.wav", text, voice)
     return send_file(speech, mimetype='audio/wav')
 
+@app.route('/stt', methods=['POST'])
+def stt():
+    audio_data = request.data
+
+    if not audio_data:
+        return jsonify({'error': 'No audio file provided'}), 400
+    with open("temp_audio.wav", "wb") as f:
+        f.write(audio_data)
+    result_text = generateText("temp_audio.wav")
+    os.remove("temp_audio.wav")
+
+    return jsonify({'result': result_text})
 
 if __name__ == '__main__':
-    app.run(debug=True) # TODO: Set debug=False before deployment
+    app.run(debug=True)
--- textToSpeechService/textToSpeech.py
+++ textToSpeechService/textToSpeech.py
@@ -1,9 +1,19 @@
 import elevenlabs as el
+from openai import OpenAI
 import os
+
 here = os.path.dirname(os.path.abspath(__file__))
+
+# ElevenLabs
 filename = os.path.join(here, 'el_api_key.txt')
 fHandle = open(filename, "r")
 api_key = fHandle.readline().strip()
+fHandle.close()
+
+# OpenAI
+filename = os.path.join(here, 'api_key.txt')
+fHandle = open(filename, "r")
+openai_api_key = fHandle.readline().strip()
 fHandle.close()
 
 el.set_api_key(api_key)
@@ -18,5 +28,17 @@ def generateSpeech(path, text, voice):
 	el.save(audio=audio, filename=full_path)
 	return full_path
 
+def generateText(path):
+	"""Generates text from the given speech file (WAV)"""
+	client = OpenAI(api_key=openai_api_key)
+
+	with open(path, "rb") as audio_file:
+		transcript = client.audio.transcriptions.create(
+		model="whisper-1", 
+		file=audio_file
+		)
+	return transcript.text
+
 if __name__ == "__main__":
-	generateSpeech("test.wav", "I know what it's like to struggle with addiction motherfuckahs!", "Leo Gura")
+	# generateSpeech("test.wav", "I know what it's like to struggle with addiction motherfuckahs!", "Leo Gura")
+	print(generateText(os.path.join(here, 'recording.m4a')))
